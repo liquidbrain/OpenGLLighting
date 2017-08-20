8e1b5c6136dbfbd919ec5a96545e5945d7024329
John Koszarek <john.koszarek@gmail.com>
2017-08-20 13:49:58 -0500

    Color is calculated using Phong reflection model.

--- OpenGLLighting/1.colors.fs
+++ OpenGLLighting/1.colors.fs
@@ -2,10 +2,37 @@
 
 out vec4 FragColor;
 
-uniform vec3 objectColor;
+in vec3 Normal;
+in vec3 FragPos;
+
+uniform vec3 lightPos;
+uniform vec3 viewPos;
 uniform vec3 lightColor;
+uniform vec3 objectColor;
 
 void main()
 {
-    FragColor = vec4(lightColor * objectColor, 1.0);
+    // Calculate ambient lighting (indirect light source(s)).
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+
+    // Calulate diffuse lighting (direct light source).
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    // Calculate specular lighting (the spot of light that appears on a shiny object; usually the
+    // color of the light rather than the color of the object).
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;
+
+    // Calculate the Phong reflection model.
+    vec3 result = (ambient + diffuse + specular) * objectColor;
+
+    // Set the fragment's color.
+    FragColor = vec4(result, 1.0);
 }
